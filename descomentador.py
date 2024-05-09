77aaef5a241d9083d17e7f8c7a2c64df95504b6d
kellyson <kellyson.medeiros.pdf@gmail.com>
2024-05-08 21:12:50 -0300

    teste

---
+++ descomentador.py
@@ -0,0 +1,15 @@
+import re
+
+def remove_comments(code):
+    code = re.sub(r'/\*.*?\*/', '', code, flags=re.DOTALL)
+    code = re.sub(r'<!--.*?-->', '', code, flags=re.DOTALL)
+    return code
+
+with open('index.html', 'r+', encoding='utf-8') as file:
+    codigo = file.read()
+    file.seek(0)  
+
+    codigo_sem_comentarios = remove_comments(codigo)
+    file.write(codigo_sem_comentarios)
+    file.truncate() 
+
